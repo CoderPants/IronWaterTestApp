48262b9cd5c12549b5a0f1273b25cc2a2bc81c02
Mikhail <wot.smailik@yandex.ru>
2020-02-17 15:05:20 +0300

    Fixed code

--- app/src/main/java/com/ironwater/testapp/model/Description.kt
+++ app/src/main/java/com/ironwater/testapp/model/Description.kt
@@ -1,7 +1,11 @@
 package com.ironwater.testapp.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Description(
     val company : String,
     val url : String,
+    @SerializedName("company_logo")
+    val companyLogo : String,
     val about : String
 )
